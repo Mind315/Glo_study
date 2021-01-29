906a3b7836c7e43f31c0425d681d92ca133a0fc6
Oleg Moskaltsov <rise22597rise@gmail.com>
2021-01-29 19:11:47 +0300

    Выполнимл урок 2

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 30000,
+   income = 'рэкет',
+   addExpenses = 'ЖКХ, Такси, учеба',
+   deposit = true,
+   mission = 500000,
+   period = 10;
 
-alert(2021);
-console.log("Hi all!");
+   console.log(typeof money);
+   console.log(typeof income);
+   console.log(typeof deposit);
+
+   console.log(addExpenses.legth);
+
+   console.log('Период равен ' + period + ' месяцев');
+   console.log('Цель заработать ' + mission + ' рублей');
+   console.log(addExpenses.toLowerCase().split(', '));  
+
+let budgetDay = money / 30;
+console.log(budgetDay);
