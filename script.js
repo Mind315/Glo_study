46f148a66ebc053701ec0d8472a29b62779917a6
Oleg Moskaltsov <rise22597rise@gmail.com>
2021-02-04 21:10:31 +0300

    Случайное число

--- script.js
+++ script.js
@@ -1,96 +1,42 @@
 "use strict";
-// ------------Проверяем! Если число - результат будет true!
-let isNumber = function (n) {
+
+const isNum = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
-//-------------- Переменные!
-let money,
-  income = "рэкет",
-  addExpenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую",
-    "ЖКХ, Такси, учеба"
-  ),
-  deposit = confirm("Есть ли у вас депозит в банке?"),
-  mission = 500000,
-  period = 10;
-let expenses = []; // <----- это массим с обязательными расходами.
 
-  //------------------- Проверка!! Ваш месячный доход!
-let start = function () {
-  
-  do {
-    money = +prompt("Ваш месячный доход?", 31000);
-  } while (!isNumber(money));
-};
-start();
-
-
-//------------------ Получаем все расходы и их названия.
-const getExpensesMonth = function () {
-  let sum = 0;
-  for (let i = 0; i < 2; i++) {
-    expenses[i] = prompt(
-      "Введите обязательную статью расходов?",
-      "Оплата интернета"
-    );
-
-    let data;
-    do {
-      data = +prompt("Во сколько это обойдется?", 2000);
-    } while (!isNumber(data));
-    sum += data;
-  }
-  console.log(expenses);
-  return sum;
-};
-//-------------------- Записали в переменную!
-let expensesAmount = getExpensesMonth();
-
-
-//-------------------------- Вычисляем сколько накопили за месяц.
-const getAccumulatedMonth = function () {
-  return money - expensesAmount;
+const randNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-let accumulatedMonth = getAccumulatedMonth();// Переменная с накоплениями
-let budgetDay = accumulatedMonth / 30;// Бюджет на день
-let missionTarget = mission / accumulatedMonth;// ????????разобраться. Это что-то осталось от прошлого кода.
-
-//--------------------------------Вычисления времени накопления цели в месяцах.
-const getTargetMonth = function () {
-  let targetMonth = Math.ceil(mission / accumulatedMonth);
-  if (targetMonth >= 0) {
-    return "Достигните цели накопления за " + targetMonth + " месяцев!!";
-  } else if (targetMonth < 0) {
-    return "Вы ничерта не накопите друг мой!";
-  }
+const guessNumber = function () {
+  let x = randNumber(1, 100);
+  const askNumber = function () {
+    let num = prompt("Угадай число от 1 до 100");
+   
+    if (num === null) {
+      alert("Игра окончена!!!");
+    } 
+    if (isNum(num)) {
+      const realNum = num;
+      if (realNum > x) {
+        alert("Загаданное число меньше");
+        askNumber();
+      } else if (realNum < x) {
+        alert("Загаданное число больше");
+        askNumber();
+      } else {
+        if (confirm("Поздравляю, Вы угадали!!!")) {
+          guessNumber();
+        } else {
+          alert("Игра окончена!!!");
+          return;
+        }
+      }
+    } else {
+      alert('Это не число. Заново!');
+      askNumber();
+    }
+  };
+  askNumber();
 };
-
-// -------------------------------Проверка на тип данных.
-const showTypeOf = function (data) {
-  console.log(data, typeof data);
-};
-//---------------------------------- Оценка, сколько денег ты зарабатываешь в день или ты бомж.
-let getStatusIncome = function () {
-  if (budgetDay > 1200) {
-    return "У вас высокий уровень дохода";
-  } else if (budgetDay >= 600 && budgetDay <= 1200) {
-    return "У вас средний уровень дохода";
-  } else if (budgetDay < 600 && budgetDay > 0) {
-    return "К сожалению у вас уровень дохода ниже среднего";
-  } else if (budgetDay <= 0) {
-    return "Что то пошло не так!";
-  }
-};
-// --------------------------------------------------Вывод в консоль!
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(`Расходы на месяц ${expensesAmount}`);
-console.log(addExpenses.toLowerCase().split(", "));
-console.log("Бюджет на месяц " + accumulatedMonth);
-console.log(getTargetMonth());
-console.log("Бюджет на день : " + Math.floor(budgetDay));
-
-console.log(getStatusIncome());
+guessNumber();
