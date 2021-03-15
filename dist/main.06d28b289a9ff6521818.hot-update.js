ea44f69e5c4fe75c08218c3d6c904bc22e791d75
Oleg Moskaltsov <rise22597rise@gmail.com>
2021-03-15 19:50:36 +0300

    fix 6

---
+++ dist/main.06d28b289a9ff6521818.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_3DGlo"]("main",{
+
+/***/ "./src/modules/sendForm.js":
+/*!*********************************!*\
+  !*** ./src/modules/sendForm.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\GloAcademy\\\\src\\\\modules\\\\sendForm.js: Unexpected token, expected \\\")\\\" (66:34)\\n\\n\\u001b[0m \\u001b[90m 64 |\\u001b[39m          postData(body)\\u001b[0m\\n\\u001b[0m \\u001b[90m 65 |\\u001b[39m            \\u001b[33m.\\u001b[39mthen((response) \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 66 |\\u001b[39m                \\u001b[36mif\\u001b[39m(response\\u001b[33m.\\u001b[39mstatus \\u001b[35m1\\u001b[39m\\u001b[33m==\\u001b[39m \\u001b[35m200\\u001b[39m) {\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m                                   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 67 |\\u001b[39m                  \\u001b[36mthrow\\u001b[39m \\u001b[36mnew\\u001b[39m \\u001b[33mError\\u001b[39m(\\u001b[32m'status network is not 200'\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 68 |\\u001b[39m                  \\u001b[0m\\n\\u001b[0m \\u001b[90m 69 |\\u001b[39m                }\\u001b[0m\\n    at Parser._raise (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:776:17)\\n    at Parser.raiseWithData (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:769:17)\\n    at Parser.raise (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:737:17)\\n    at Parser.unexpected (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9686:16)\\n    at Parser.expect (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9672:28)\\n    at Parser.parseHeaderExpression (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12474:10)\\n    at Parser.parseIfStatement (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12555:22)\\n    at Parser.parseStatementContent (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12247:21)\\n    at Parser.parseStatement (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12202:17)\\n    at Parser.parseBlockOrModuleBlockBody (C:\\\\GloAcademy\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12782:25)\");\n\n//# sourceURL=webpack://3DGlo/./src/modules/sendForm.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("122d25bdd8111c88de8f")
+/******/ })();
+/******/ 
+/******/ }
+);
